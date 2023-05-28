dd5308a5f06f17f1513ec5415341cdeef74875da
voduycuong <voduycuonggli@gmail.com>
2023-05-28 22:42:05 +0700

    bugs fixed

--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -1,14 +1,9 @@
 package main.java;
 
 public class Record extends Item {
-    private String genre;
-
+    
     public Record(String itemId, String title, String rentType, String loanType, int numberOfCopies, double rentalFee, String genre) {
         super(itemId, title, "Record", loanType, numberOfCopies, rentalFee, genre);
-        this.genre = genre;
     }
 
-    public String getGenre() {
-        return genre;
-    }
 }
